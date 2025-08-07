f20061296d3528bf29670c5918720ca644bd23d6
stormfather0 <ivan.ystskovyna@gmail.com>
2025-08-07 11:17:47 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-app/', 
+  base: '/my-app/', 
   plugins: [react(),
     tailwindcss(),
   ],
